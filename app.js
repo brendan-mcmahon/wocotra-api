e2f5f56777fd5104ae52ba57413152577174226c
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2021-11-30 09:06:50 -0500

    fix port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 let cors = require("cors");
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 var corsOptions = {
     origin: "http://localhost:3000"
